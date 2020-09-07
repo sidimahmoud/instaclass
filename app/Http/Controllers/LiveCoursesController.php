4aaecbe9d2d46a0e7d8b2f93971c77cf6ca42a6d
MedAb94 <medab.vall@gmail.com>
2020-09-07 13:13:24 +0000

    live courses

--- app/Http/Controllers/LiveCoursesController.php
+++ app/Http/Controllers/LiveCoursesController.php
@@ -13,8 +13,8 @@ class LiveCoursesController extends Controller
     {
         // Substitute your Twilio Account SID and API Key details
         $accountSid = env('TWILIO_ACCOUNT_SID');
-        $apiKeySid = env('TWILIO_API_KEY_SID');
-        $apiKeySecret = env('TWILIO_API_KEY_SECRET');
+        $apiKeySid = env('TWILIO_API_KEY');
+        $apiKeySecret = env('TWILIO_API_SECRET');
 
         $identity = uniqid();
 
