d93a6d7e74624f67c37ee92f8928a34982033548
MedAb94 <medab.vall@gmail.com>
2020-09-08 22:01:28 +0000

    live courses

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::middleware('auth:sanctum')->group(function () {
         //Live courses
         Route::get('/access_token', 'LiveCoursesController@generate_token');
         Route::post('/endroom/{myRoom}', 'LiveCoursesController@closeRoom');
-        Route::get('/room/{myRoom}', 'LiveCoursesController@closeRoom');
+        Route::get('/room/{myRoom}', 'LiveCoursesController@roomDetails');
     });
 
     Route::get('/user', 'Admin\UsersController@show');
