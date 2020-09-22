8f145ecb474b8c904530437c34fb9e7fd446f77d
MedAb94 <medab.vall@gmail.com>
2020-09-22 14:30:05 +0000

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 //        $this->call(CategoriesTableSeeder::class);
-        //$this->call(CourseStatusSeeder::class);
-        //$this->call(CoursesTableSeeder::class);
-        //$this->call(EnrollmentsSeeder::class);
-        //$this->call(RatingsSeeder::class);
+        $this->call(CourseStatusSeeder::class);
+        $this->call(CoursesTableSeeder::class);
+        $this->call(EnrollmentsSeeder::class);
+        $this->call(RatingsSeeder::class);
     }
 }
