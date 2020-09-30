9c0f914af0cfe0acddfbb2ef5d5755b40db5dceb
MedAb94 <medab.vall@gmail.com>
2020-09-30 19:55:36 +0000

    Email notification

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -15,19 +15,15 @@ class CategoriesTableSeeder extends Seeder
     {
         Category::truncate();
         Category::create([
-            'name' => 'WEB DEVELOPMENT',
+            'name_en' => 'WEB DEVELOPMENT',
+            'name_fr' => 'DEVELOPPMENT WEB',
             'description' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
             'image' => 'https://img-a.udemycdn.com/course/750x422/147660_d5ac_5.jpg',
             'created_at' => Carbon::now(),
         ]);
         Category::create([
-            'name' => 'DATA SCIENCE',
-            'description' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
-            'image' => 'https://img-a.udemycdn.com/course/750x422/147660_d5ac_5.jpg',
-            'created_at' => Carbon::now(),
-        ]);
-        Category::create([
-            'name' => 'CLOUD COMPUTING',
+            'name_en' => 'DATA SCIENCE',
+            'name_fr' => 'SCIENCE DES DONNEES',
             'description' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
             'image' => 'https://img-a.udemycdn.com/course/750x422/147660_d5ac_5.jpg',
             'created_at' => Carbon::now(),
