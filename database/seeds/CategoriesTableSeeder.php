090461a1668668cf180b55e9bfd051b61653ff8c
MedAb94 <medab.vall@gmail.com>
2020-09-30 20:02:48 +0000

    Email notification

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -17,14 +17,12 @@ class CategoriesTableSeeder extends Seeder
         Category::create([
             'name_en' => 'WEB DEVELOPMENT',
             'name_fr' => 'DEVELOPPMENT WEB',
-            'description' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
             'image' => 'https://img-a.udemycdn.com/course/750x422/147660_d5ac_5.jpg',
             'created_at' => Carbon::now(),
         ]);
         Category::create([
             'name_en' => 'DATA SCIENCE',
             'name_fr' => 'SCIENCE DES DONNEES',
-            'description' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
             'image' => 'https://img-a.udemycdn.com/course/750x422/147660_d5ac_5.jpg',
             'created_at' => Carbon::now(),
         ]);
