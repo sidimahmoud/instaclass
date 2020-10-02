b8c5730bda23f917594e0319dea18408038dfb02
MedAb94 <medab.vall@gmail.com>
2020-10-02 18:51:15 +0000

    profile update

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/room-recordings/{roomSid}', 'LiveCoursesController@roomRecordings');
         Route::get('/access_token/{muRoom}/{user}', 'LiveCoursesController@generate_token');
 
-    Route::resource('/user', 'Admin\UsersController@show');
+    Route::resource('/user', 'Admin\UsersController');
     Route::get('/user/enrollments', 'EnrollmentController@userEnrollments');
     Route::post('/enroll', 'EnrollmentController@store');
     Route::post('/pay', 'Admin\PayementController@paymentProcess');
