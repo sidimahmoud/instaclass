955673f5c0cf12b4d6a71053dc23d9eaddef3446
Mokhtar sidi mahmoud <sidmah10@gmail.com>
2020-11-16 23:30:43 +0000

    changes 2

--- config/session.php
+++ config/session.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'encrypt' => false,
+    'encrypt' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -125,10 +125,7 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
-    ),
+    'cookie' => 'laravelsession',
 
     /*
     |--------------------------------------------------------------------------
